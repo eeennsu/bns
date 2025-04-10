9c0cb74038fd56b29832a29394b2082e0acaf5f4
eeennsu <diso592@naver.com>
2025-04-10 18:16:35 +0900

    chore: Add github actions for deploy at stg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+
+const isStg = (process.env.STAGE = 'github')
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    output: 'export',
+    ...(isStg && {
+        basePath: '/bnc',
+        assetPrefix: '/bnc/',
+    }),
+}
 
-export default nextConfig;
+export default nextConfig
