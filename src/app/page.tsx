113b5e8099e920e19757f1cfd68a7d185edaa899
eeennsu <diso592@naver.com>
2025-04-21 21:37:20 +0900

    feat: 테스트 커밋

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-'use client'
-
 import { FC } from 'react'
 
 const HomePage: FC = () => {
     return (
         <main>
             <h1 className='bg-black text-white'>Hello BNS!</h1>
-            <span className='text-xs font-bold text-blue-400'>{process.env.NEXT_PUBLIC_API_URL} </span>
         </main>
     )
 }
