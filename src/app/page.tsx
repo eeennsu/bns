ebb2d18fdf13290d2ea9434e93069276efedca37
eeennsu <diso592@naver.com>
2025-04-21 22:10:05 +0900

    feat: 커밋 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ const HomePage: FC = () => {
     return (
         <main>
             <h1 className='bg-black text-white'>Hello BNS!</h1>
+            <h2>이제 시작이야 ~ 내꿈을 ~~ 피!까츄~ 언제언제 까지나~</h2>
             <span className='text-xs font-bold text-blue-400'>{process.env.NEXT_PUBLIC_API_URL} </span>
-            hello world!!!
         </main>
     )
 }
