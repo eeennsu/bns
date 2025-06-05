4dfdcf99bc797d446116c0b300f0acb015bd1d65
eeennsu <diso592@naver.com>
2025-06-05 20:55:46 +0900

    feature: 빌드 스크립트 추가

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+if [[ "$VERCEL_GIT_COMMIT_REF" == "beta" || "$VERCEL_GIT_COMMIT_REF" == "dev" ]] ; then
+  # 빌드 진행
+  echo "✅ - Build can proceed"
+  exit 1;
+else
+  # 빌드 중단
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
