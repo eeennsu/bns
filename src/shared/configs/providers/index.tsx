3c8074ebcaa85841b28d4dd2ddf6afe31e88125a
eeennsu <diso592@naver.com>
2025-06-08 10:15:09 +0900

    fix: 파일 이름 복원

--- src/shared/configs/providers/index.tsx
+++ src/shared/configs/providers/index.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 import SonnerToaster from './SonnerToaster';
-import TanstackQueryProvider from './TanstackQuery';
+import TanstackQueryProvider from './tanstackQuery';
 
 const ConfigProviders = ({ children }: { children: ReactNode }) => {
   return (
