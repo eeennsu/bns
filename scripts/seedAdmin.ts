01cd2c188ee8703d57b2147b3c49cd3e558f3ce5
eeennsu <diso592@naver.com>
2025-06-28 12:25:55 +0900

    feature: seedadmin 수정

--- scripts/seedAdmin.ts
+++ scripts/seedAdmin.ts
@@ -1,6 +1,9 @@
 import db from '@db/index';
 import { users } from '@db/schemas/users';
 import bcrypt from 'bcryptjs';
+import { eq } from 'drizzle-orm';
+
+const ADMIN_NAME = '엄준식';
 
 const seedAdmin = async () => {
   const password = process.env.SEED_ADMIN_PW;
@@ -12,14 +15,25 @@ const seedAdmin = async () => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
+    const existingAdmin = await db
+      .select()
+      .from(users)
+      .where(eq(users.username, ADMIN_NAME))
+      .limit(1);
+
+    if (existingAdmin) {
+      throw new Error(`Existing admin name : ${ADMIN_NAME}`);
+    }
+
     await db.insert(users).values({
-      username: '엄준식',
+      username: ADMIN_NAME,
       password: hashedPassword,
       role: 'admin',
       createdAt: new Date(),
     });
   } catch (error) {
     console.error('seedAdmin error: ', error);
+    throw error;
   }
 };
 
