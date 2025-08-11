73adbb69eb9ffc52124b579fb1bdc04c77300e62
eeennsu <diso592@naver.com>
2025-08-11 20:36:11 +0900

    feature: 모달 테스트

--- src/app/(main)/product/layout.tsx
+++ src/app/(main)/product/layout.tsx
@@ -1,18 +1,14 @@
 import { Suspense, type FC, type PropsWithChildren, type ReactNode } from 'react';
 
-import ModalShell from './ModalShell';
-
 interface IProps {
   modal: ReactNode;
 }
 
 const ProductLayout: FC<PropsWithChildren<IProps>> = ({ children, modal }) => {
   return (
-    <main className='!lg:gap-20 container !gap-10'>
+    <main className='container !gap-10 lg:!gap-20'>
       {children}
-      <Suspense fallback={null}>
-        <ModalShell>{modal}</ModalShell>
-      </Suspense>
+      <Suspense fallback={null}>{modal}</Suspense>
     </main>
   );
 };
